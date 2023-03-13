50d36855867fa8f6b8c2956cc0408fdf4e8f0ae6
Joel Parra <joelramonph@gmail.com>
2023-03-13 10:23:50 -0400

    add conditionals

--- script/script.js
+++ script/script.js
@@ -1,12 +1,22 @@
 let counter = 0
-const numero = [1,2,20,4,5]
+
 const value = document.querySelector('#value')
 const btns = document.querySelectorAll('.btn')
 
 btns.forEach(function(btn) {
     btn.addEventListener("click", function (e) {
-        console.log(e.currentTarget)
+        const styles = e.currentTarget.classList
+        if(styles.contains('decrease')){
+            counter--
+        } else if(styles.contains('increase')) {
+            counter++
+        } else {
+            counter = 0
+        }
+
+        value.textContent = counter
     })
 })
+
 
 
